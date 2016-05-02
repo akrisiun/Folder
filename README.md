d1a8c2595e094c029526497a08be5e2b711ed068
AndriusK <akrisiun@gmail.com>
2016-05-02 11:54:48 +0300

    Folder app init

---
+++ README.md
@@ -0,0 +1,8 @@
+## C# Win32 shell folder application
+
+```
+git clone https://github.com/akrisiun/Folder.git
+cd Folder
+git submodule add -f https://github.com/akrisiun/MultiSelectTreeView.git src/MultiSelectTreeView
+git submodule add -f https://github.com/akrisiun/SharpShell.git src/SharpShell
+```
---
+++ src/Folder/FS/FileSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace VfpProj
+{
+    public static class FileSystem
+    {
+        public static string CurrentDirectory {
+            get { return Directory.GetCurrentDirectory(); } // Environment.CurrentDirectory; } 
+            set { System.IO.Directory.SetCurrentDirectory(value); }
+        }
+
+
+    }
+}
